95b5e0e7291a9494ff779ace4ed951507635c8cb
Kaue Castro <kaue.castro@era-br.com>
2018-03-01 13:35:59 -0300

    Product CRUD

--- BeerTemperature/DAO/ProductDAO.cs
+++ BeerTemperature/DAO/ProductDAO.cs
@@ -19,33 +19,74 @@ namespace BeerTemperature.DAO
             }
         }
 
-
-
         public Product Read(Product product)
         {
             if (product == null)
                 throw new Exception("Product is required to cretate in database !");
 
             Product productResponse = new Product();
-                using (var context = new EntityContext())
-                {
-                    var products = from p in context.Products select p;
+            using (var context = new EntityContext())
+            {
+                var products = from p in context.Products select p;
 
-                    //Filters
-                    if (product.id > 0)
-                        products = products.Where(q => q.id == product.id);
-                    if (product.productName != null && !string.IsNullOrEmpty(product.productName))
-                        products = products.Where(q => q.productName == product.productName);
+                if (product.id > 0)
+                    products = products.Where(q => q.id == product.id);
+                if (product.productName != null && !string.IsNullOrEmpty(product.productName))
+                    products = products.Where(q => q.productName == product.productName);
 
-                    productResponse = products.FirstOrDefault();
-                }
+                if (products == null)
+                    throw new Exception("There are no products in the database with this id !");
+
+                productResponse = products.FirstOrDefault();
+            }
 
             return productResponse;
         }
 
+        public void UpdateById(Product product)
+        {
+            if (product == null)
+                throw new Exception("Product is required to cretate in database !");
 
+            Product productResponse = new Product();
 
+            using (var context = new EntityContext())
+            {
+                var products = from p in context.Products select p;
 
+                if (product.id > 0)
+                    products = products.Where(q => q.id == product.id);
 
+                if (products == null)
+                    throw new Exception("There are no products in the database with this id !");
+
+                productResponse = products.FirstOrDefault();
+                productResponse.productName = product.productName;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            if (product == null)
+                throw new Exception("Product is required to cretate in database !");
+
+            Product productResponse = new Product();
+
+            using (var context = new EntityContext())
+            {
+                var products = from p in context.Products select p;
+
+                if (product.id > 0)
+                    products = products.Where(q => q.id == product.id);
+
+                if (products == null)
+                    throw new Exception("There are no products in the database with this id !");
+
+                productResponse = products.FirstOrDefault();
+                context.Products.Remove(productResponse);
+                context.SaveChanges();
+            }
+        }
     }
 }
